6e57fd80208d2a379fe41749d554a768382f7145
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-30 01:30:25 +0700

    Add files via upload

---
+++ Program Untuk Mengetahui Segitiga Sama Sisi atau Bukan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float sudut,jawab;
+	menu :
+	cout<<"masukan besarnya segitiga :";
+	cin>>sudut;
+	if(sudut==60)
+	
+	cout<<"\njenis segitiga adalah sama sisi\n\n"<<"--------------------\n\n";
+	else
+	cout<<"\njenis segitiga bukan sama sisi\n\n"<<"--------------------\n\n";
+	
+}
